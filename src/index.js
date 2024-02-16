790fd7ffebea14ac51d8a3bda85727e334bda918
millemaliu <millemal@iu.edu>
2024-02-15 19:26:50 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+const { add } = require("./arithmetica");
+
 const port = 3000;
 
 
@@ -12,8 +14,24 @@ app.get('/', (req, res) => {
 
 
 app.get('/add/:n/:m', (req, res) => {
-    res.json(Number(req.params.n) + Number(req.params.m));
+    res.json(add(Number(req.params.n), Number(req.params.m)));
 });
+
+
+app.get('/subtract/:n/:m', (req, res) => {
+    res.json(subtract(Number(req.params.n), Number(req.params.m)));
+});
+
+
+app.get('/multiply/:n/:m', (req, res) => {
+    res.json(multiply(Number(req.params.n), Number(req.params.m)));
+});
+
+
+app.get('/divide/:n/:m', (req, res) => {
+    res.json(divide(Number(req.params.n), Number(req.params.m)));
+});
+
 
 
 app.listen(port);
